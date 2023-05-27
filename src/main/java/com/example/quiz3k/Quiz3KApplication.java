59b44d984c983b08917f84a9b3fc0b0f8fd2e5e7
Mateusz Komar <mateusz.komar1988@gmail.com>
2023-05-27 14:19:51 +0200

    Initial commit, create project -first commit

---
+++ src/main/java/com/example/quiz3k/Quiz3KApplication.java
@@ -0,0 +1,13 @@
+package com.example.quiz3k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz3KApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Quiz3KApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quiz3k/Quiz3KApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiz3k;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz3KApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
