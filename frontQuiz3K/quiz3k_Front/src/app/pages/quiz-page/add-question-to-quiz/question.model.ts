3c2ab03e6fcffb6dddac425895ce08d0b605e246
Mateusz Komar <mateusz.komar1988@gmail.com>
2023-06-17 23:51:54 +0200

    modification some style css

--- frontQuiz3K/quiz3k_Front/src/app/pages/quiz-page/add-question-to-quiz/question.model.ts
+++ frontQuiz3K/quiz3k_Front/src/app/pages/quiz-page/add-question-to-quiz/question.model.ts
@@ -1,8 +1,6 @@
 export interface Question {
   id: number;
   questionText: string;
-  // answers: Answer[];
-  // correctAnswerId: string;
   questionType: string;
   questionQuizId: string;
   answers?: Answer[];
@@ -12,7 +10,6 @@ export interface Answer {
   id: number;
   answerForTheQuestion: string;
   confirmedAnswer: boolean;
-  // deleted?: boolean;
   answerQuestionId: number;
 
 }
