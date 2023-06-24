384ff8f51c579844f247eb3344e1047749d6e99b
Patryk <patryk.marcisz.v2@gmail.com>
2023-06-24 10:00:56 +0200

    obsluga dowolnego exception i rzucanie 500 bad request w odpowiedzi dla CopyQuizController

--- src/main/java/com/example/quiz3k/controller/CopyQuizController.java
+++ src/main/java/com/example/quiz3k/controller/CopyQuizController.java
@@ -4,12 +4,15 @@ import com.example.quiz3k.model.dto.QuizData;
 import com.example.quiz3k.repository.CopyQuizRepository;
 import com.example.quiz3k.service.CopyQuizService;
 import com.example.quiz3k.service.QuestionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@Slf4j
 public class CopyQuizController {
 
     private final CopyQuizService copyService;
@@ -24,4 +27,11 @@ public class CopyQuizController {
         copyService.saveQuiz(quizData);
         return ResponseEntity.ok("Quiz został zapisany pomyślnie");
     }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public void handleException(Exception e){
+        log.error(e.getMessage());
+    }
+
 }
