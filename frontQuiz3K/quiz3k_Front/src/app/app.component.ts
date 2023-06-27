1841fcac0f20c15c9b0bf4ece51cb797f4ff35ff
Mateusz Komar <mateusz.komar1988@gmail.com>
2023-06-27 16:43:26 +0200

    added login security and empty list of quiz message

--- frontQuiz3K/quiz3k_Front/src/app/app.component.ts
+++ frontQuiz3K/quiz3k_Front/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FirstPageComponent } from './pages/first-page.component';
+import {Router} from "@angular/router";
+import {AuthService} from "./pages/auth-page/auth.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,13 @@ import { FirstPageComponent } from './pages/first-page.component';
 })
 export class AppComponent {
   title = 'quiz3k_Front';
+  constructor(private router: Router, private authService: AuthService) {}
+
+  getRedirectLink(): string {
+    if (this.authService.isLoggedUser()) {
+      return '/quiz';
+    } else {
+      return '';
+    }
+  }
 }
